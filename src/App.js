e9136af7369b6a42bcfea11a48682396e0a0e71c
chetan.accord <Chetan.s@accord-soft.com>
2024-05-07 10:42:29 +0530

    set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Welcome from "./views/welcome";
-import Card from "./views/card";
+import Login from "./views/login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Card />} />
-        {/* <Route path="/card" element={<Card />} /> */}
+        <Route path="/" element={<Login />} />
 
         <Route path="*" element={<></>} />
       </Routes>
